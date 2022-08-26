37cec4d9ade1b0fc624061736c71a4f2bb0631bb
Jordan McRae <jdmcrae8@gmail.com>
2022-08-26 12:00:40 -0400

    Add test for slash commands

--- test/cli/slashCommands.js
+++ test/cli/slashCommands.js
@@ -19,7 +19,8 @@ const { file } = chaiFiles;
 
 const runCLI = (args) => run('npx discord-bot', args);
 
-const MOCK_EVENT_PATH = path.join(__dirname, '../../src/events/messages/mochaTestCommand.js');
+const MOCK_EVENT_PATH = path.join(__dirname, '../../src/events/messages/mochaTestEventHandler.js');
+const MOCK_SLASH_COMMAND_PATH = path.join(__dirname, '../../src/slashCommands/mochaTestCommand.js');
 
 describe('Slash Commands', function() {
   it('Runs', async () => {
@@ -47,7 +48,7 @@ describe('Slash Commands', function() {
       // Select "Messages" event type
       ENTER,
       // Name the command
-      'mochaTestCommand',
+      'mochaTestEventHandler',
       ENTER,
       // Do not open file
       'n',
@@ -59,7 +60,54 @@ describe('Slash Commands', function() {
     return expect(file(MOCK_EVENT_PATH)).to.exist;
   });
 
+  it ('Can create a slash command with options', async() => {
+    await runCLI([
+      // Select "Create an event handler"
+      DOWN,
+      DOWN,
+      DOWN,
+      DOWN,
+      ENTER,
+      // Name the command
+      'mocha-test-command',
+      ENTER,
+      // Describe the command
+      'My test command',
+      ENTER,
+      // Add an option
+      'y',
+      // Select string
+      ENTER,
+      // Name the option
+      'option-one',
+      ENTER,
+      // Describe the option
+      'A test option',
+      ENTER,
+      // Is required
+      'y',
+      ENTER,
+      // Add predefined choices
+      'y',
+      ENTER,
+      // Add choices
+      'Apple, Banana, Coconut, Passion Fruit',
+      ENTER,
+      // No more options
+      'n',
+      ENTER,
+      // Do not open file
+      'n',
+      ENTER,
+      // End process
+      EXIT
+    ]);
+
+    return expect(file(MOCK_SLASH_COMMAND_PATH)).to.exist;
+  });
+
     after(() => {
       fs.unlinkSync(MOCK_EVENT_PATH);
+      fs.unlinkSync(MOCK_SLASH_COMMAND_PATH);
     });
 });
